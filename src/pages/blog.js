c5203d47ed1fd16d1d425d28cad339eb0ad29a73
田原 一樹 <ssp0727.lnc@gmail.com>
2019-12-05 22:13:17 +0900

    [update] add css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
+import '../components/blog.css'
 
 export default class blog extends Component {
   render() {
     return (
       <Layout>
-        <div>
+        <h1 className="blog-title">
           this is our blog page
-        </div>
+        </h1>
       </Layout>
     )
   }
