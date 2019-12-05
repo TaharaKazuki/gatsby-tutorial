400c1f528179e3f19543dd6b8de2591f80bf91d5
田原 一樹 <ssp0727.lnc@gmail.com>
2019-12-05 22:27:43 +0900

    [update] add css module

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
-import '../components/blog.css'
+import styles from  '../components/blog.module.css'
 
 export default class blog extends Component {
   render() {
     return (
       <Layout>
-        <h1 className="blog-title">
-          this is our blog page
-        </h1>
+        <div className={styles.blog}>
+          <h1 className={styles.title}>this is our blog page</h1>
+          <h2>this second heading</h2>
+        </div>
       </Layout>
     )
   }
